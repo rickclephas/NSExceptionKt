33e5de6fe7d5dd2d1239b5a38ed5d34a29428900
Rick Clephas <rclephas@gmail.com>
2024-05-22 22:22:11 +0200

    Bump version to 1.0.0-BETA-4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
 allprojects {
     group = "com.rickclephas.kmp"
-    version = "1.0.0-BETA-3-kotlin-2.0.0-RC3"
+    version = "1.0.0-BETA-4"
 
     repositories {
         mavenCentral()
