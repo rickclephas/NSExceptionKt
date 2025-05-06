7114aa9d7a9b2cb6acee535420f95a82fdd06249
Rick Clephas <rclephas@gmail.com>
2025-05-06 18:31:13 +0200

    Update Kotlin to 2.1.21-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
 allprojects {
     group = "com.rickclephas.kmp"
-    version = "1.0.0-BETA-11-kotlin-2.1.21-RC"
+    version = "1.0.0-BETA-11-kotlin-2.1.21-RC2"
 
     repositories {
         mavenCentral()
