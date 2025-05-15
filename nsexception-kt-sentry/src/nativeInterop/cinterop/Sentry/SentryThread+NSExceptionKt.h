343a1587fde598cac44ffcd4055e3dd0bbc5cc78
Rick Clephas <rclephas@gmail.com>
2025-05-15 16:29:12 +0200

    Remove deprecated bugsnag, crashlytics and sentry implementations

--- nsexception-kt-sentry/src/nativeInterop/cinterop/Sentry/SentryThread+NSExceptionKt.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <SentryThread.h>
-
-// When we create the NSNumber in Kotlin it isn't converted to a boolean,
-// so we are using this wrapper function instead.
-void NSExceptionKt_SentryThreadSetCrashed(SentryThread *thread) {
-    thread.crashed = @(YES);
-}
